13c1f629d64b276e9a5c0e33c798f32b095092b7
johnhhancock <hancockherbertjohn@gmail.com>
2020-05-08 21:56:28 -0400

    Update mongo_helpers.py

--- mongo_helpers.py
+++ mongo_helpers.py
@@ -6,21 +6,26 @@ class mongo:
         self.client = pymongo.MongoClient()
         self.database = self.client[database]
         self.collection = self.database[collection]
-    
+
     def insert_one(self, payload: dict):
         self.collection.insert_one(payload).inserted_id
 
-    def query_match(self, column: str, value: str):
-        query = { column: value}
+    def query_match(self, column, value):
+        query = {column: value}
         return self.collection.find(query)
-    
+
     def update(self, query: str, new_values: dict):
-        # Example: 
+        # Example:
         # query = { "address": "Valley 345" }
         # new_values = { "$set": { "address": "Canyon 123" } }
         return self.collection.update_one(query, new_values)
-    
+
+
 if __name__ == '__main__':
     db = mongo(database='mydbs', collection='price_checker')
-    print(db.query_match(column='target', value=175))
-    
+    results = db.query_match(column='target', value=175)
+    records = list
+    for result in results:
+        records.append(result)
+
+    print(records)
